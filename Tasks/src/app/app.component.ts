53f70beb9bd2d03415690bdfcc787416c826f247
Mateo Nihil <mateo.nihil@gmail.com>
2025-01-14 05:26:50 -0500

    Tasks - Create

--- Tasks/src/app/app.component.ts
+++ Tasks/src/app/app.component.ts
@@ -1,21 +1,41 @@
 import { TasksCrudService } from './services/tasks.crud.service'; // Servicio
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Necesario para implementar angular en el HTML
+import { ReactiveFormsModule } from '@angular/forms'; // Formularios reactivos
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   //styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
 
   tasks : any[] = [];
+  form : FormGroup;
 
-  constructor(private tasksCrudService: TasksCrudService) { }
+  constructor(private tasksCrudService: TasksCrudService) {
+    this.form = new FormGroup({
+      title: new FormControl(''),
+      priority: new FormControl(''),
+      dueDate: new FormControl(''),
+      description: new FormControl('')
+    });
+  }
 
   ngOnInit() {
+    this.readTasks();
+  }
+
+  //Create
+  //id : number, title : string, priority : string, dueDate : string, description : string
+  createTask(): void{
+    const { title, priority, dueDate, description } = this.form.value;
+    const id = parseInt(this.generateId());
+    this.tasksCrudService.createTask(id, title, priority, dueDate, description).subscribe({});
     this.readTasks();
   }
 
@@ -23,7 +43,6 @@ export class AppComponent implements OnInit{
   readTasks(): void{
     this.tasksCrudService.readTasks().subscribe({
       next: (tasks) => {
-        console.log(tasks);
         this.tasks = tasks;
       },
       error: (error) => {
@@ -32,16 +51,24 @@ export class AppComponent implements OnInit{
     });
   }
 
-  createTask(id : number, title : string, priority : string, dueDate : string, description : string): void{
-    this.tasksCrudService.createTask(id, title, priority, dueDate, description).subscribe(() => {});
-  }
-
   updateTask(id : number, title : string, priority : string, dueDate : string, description : string): void{
     this.tasksCrudService.updateTask(id, title, priority, dueDate, description).subscribe(() => {});
   }
 
   deleteTask(id : number): void{
     this.tasksCrudService.deleteTask(id).subscribe(() => {});
+  }
+
+  generateId(): string{
+    let id = localStorage.getItem("idCounter");
+    if (id == null) {
+      localStorage.setItem("idCounter", "0");
+      id = "0";
+    }else{
+      localStorage.setItem("idCounter", (parseInt(id) + 1).toString());
+      id = (parseInt(id) + 1).toString();
+    }
+    return id;
   }
 
   title = 'Tasks';
--- Tasks/src/app/services/tasks.crud.service.ts
+++ Tasks/src/app/services/tasks.crud.service.ts
@@ -10,16 +10,17 @@ export class TasksCrudService {
 
   //Create
   //localStorage.setItem("task", JSON.stringify([{id: 1, title: "Task 1", priority: "low", dueDate: "2026-01-01", description: "This is a task"}]));
-  createTask(id: number=0, title: string="", priority: string="low", dueDate: string="", description: string=""): Observable<any> {
-    const task = [{id: 1, title: "Task 1", priority: "low", dueDate: "2026-01-01", description: "This is a task"}]
-    localStorage.setItem("task", JSON.stringify(task));
+  createTask(id_: number=0, title_: string="", priority_: string="low", dueDate_: string="", description_: string=""): Observable<any> {
+    const tasks = JSON.parse(localStorage.getItem("task") || "[]");
+    tasks.push({id:id_, title:title_, priority:priority_, dueDate:dueDate_, description:description_});
+    localStorage.setItem("task", JSON.stringify(tasks));
     return new Observable<any>();
   }
   //Read
   readTasks(): Observable<any> {
     const tasksString = localStorage.getItem("task") || "[]";
     const tasks = JSON.parse(tasksString);
-    console.log(tasks);
+    //console.log(tasks);
     return of(tasks);
   }
   //Update
